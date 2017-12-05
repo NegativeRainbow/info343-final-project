d3851dd81d139560769081675f34247aebd2c871
Robin Yang <robinyang@Robins-MacBook-Air.local>
2017-12-05 02:55:59 -0800

    Updated Button UI

--- src/Card.js
+++ src/Card.js
@@ -3,35 +3,49 @@ import { UncontrolledCarousel } from 'reactstrap';
 import { StyleSheet, css } from 'aphrodite';
 
 const likeAnimation = {
-    'from': {transform: 'rotate(10deg)', opacity: 1 },
-    'to': { transform: 'rotate(65deg)', opacity: 0 },
+    'from': {transform: 'rotate(10deg)', opacity: 1, left: '0px', top: '0px' },
+    'to': { transform: 'rotate(65deg)', opacity: 0, left: '200px', top: '150px' }
   };
   
 const nopeAnimation = {
-    'from': {transform: 'rotate(-10deg)', opacity: 1 },
-    'to': {transform: 'rotate(-65deg)', opacity: 0 },
+    'from': {transform: 'rotate(-10deg)', opacity: 1, right: '0px', top: '0px' },
+    'to': {transform: 'rotate(-65deg)', opacity: 0, right: '200px', top: '150px' },
   };
 
+  // Button URLS from https://codepen.io/arjentienkamp/
 const styles = StyleSheet.create({
     btnLike: {
         fontSize: 24,
         color: 'white',
-        backgroundColor: '#3cb371',
+        backgroundColor: '#FFF',
         borderRadius: '100%',
-        border: 'transparent',
-        width: 56,
-        height: 56,
+        background: 'url(http://web.arjentienkamp.com/codepen/tinder/heart.png)',
+        backgroundSize: '30px',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        border: '5px solid #f0f0f0',
+        borderRadius: 50,
+        width: 50,
+        height: 50,
         display: 'inline-block',
-        boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 10px, rgba(0, 0, 0, 0.23) 0px 3px 10px',
-        ':active': {
-            boxShadow: 'rgba(0, 0, 0, 0.19) 0px 10px 30px, rgba(0, 0, 0, 0.23) 0px 6px 10px'
-        },
-        outline: 0
+        boxShadow: '1px 1px 1px 0px #e9e9e9',
+        outline: 1,
+        margin: 'auto',
+        display: 'block'
     },
     btnNope: {
-        color: 'white',
-        backgroundColor: '#ff0000'
-
+        background: 'url(http://web.arjentienkamp.com/codepen/tinder/delete.png)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '25px',
+        backgroundPosition: 'center',
+    },
+    btnOwner: {
+        background: 'url(http://web.arjentienkamp.com/codepen/tinder/info.png)',
+        width: 56,
+        height: 56,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '30px',
+        backgroundPosition: 'center',
     },
     cardNope: {
         animationName: nopeAnimation,
@@ -41,8 +55,13 @@ const styles = StyleSheet.create({
         animationName: likeAnimation,
         animationDuration: '1s',
     },
-    cardStandard: {
-        height: '475px'
+    carousel: {
+        display: 'block',
+        margin: 'auto',
+        border: '1px solid #a6a9aa',
+        borderRadius: '8px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
     }
 }
 )
@@ -66,7 +85,6 @@ export default class Card extends Component {
         })
 
         let cardAnimation = css(
-            styles.cardStandard,
             this.props.liked && styles.cardLike,
             this.props.disliked && styles.cardNope
         );
@@ -74,21 +92,23 @@ export default class Card extends Component {
             <div className='d-flex justify-content-center'>
                 <div className={"card " + cardAnimation}>
                     <div className="card-body">
-                        <div className="container">
-                            <UncontrolledCarousel
+                            <UncontrolledCarousel className={css(styles.carousel)}
                                 items={carouselItems}
                                 indicators={false}
                                 controls={true} />
-                        </div>
+
                         <h3 className="card-title">{dogObj.name}</h3>
                         <p className="card-text">{dogObj.sex + ' ' + dogObj.breed}</p>
                         <p className='card-text'>{dogObj.bio}</p>
                         <div className='row'>
-                            <div className='col text-left'>
-                                <button className={css(styles.btnLike, styles.btnNope)} onClick={(event) => this.props.onNopeCallback(event)}>X</button>
+                            <div className='col justify-content-center'>
+                                <button className={css(styles.btnLike, styles.btnNope)} onClick={(event) => this.props.onNopeCallback(event)}></button>
                             </div>
-                            <div className='col text-right'>
-                                <button className={css(styles.btnLike)} onClick={(event) => this.props.onLikeCallback(event)}>{'<3'}</button>
+                            <div className='col justify-content-center'>
+                                <button className={css(styles.btnLike, styles.btnOwner)}></button>
+                                </div>
+                            <div className='col justify-content-center'>
+                                <button className={css(styles.btnLike)} onClick={(event) => this.props.onLikeCallback(event)}></button>
                             </div>
                         </div>
                     </div>
