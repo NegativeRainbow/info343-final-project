5246c739577b67d31bfbc5e6553f7e97209f6824
Robin Yang <robinyang@D-10-18-219-64.dhcp4.washington.edu>
2017-12-07 18:57:05 -0800

    card fixes

--- src/Card.js
+++ src/Card.js
@@ -54,6 +54,7 @@ export default class Card extends Component {
         this.state = {
             morePets: this.props.noMorePets,
         }
+        console.log(this.props.user);
     }
 
     componentDidMount() {
@@ -113,16 +114,16 @@ export default class Card extends Component {
 
     render() {
         // For ease of use
-        let dogObj = this.props.dog;
-        let ownerObj = this.props.owner;
+        let dogObj = this.props.user.pet;
+        let ownerObj = this.props.user.owner;
         console.log(dogObj);
-        let petCarouselItems = dogObj.images.map(function (img) {
-            let obj = { src: 'url(' + img + ')', altText: dogObj.name, caption: '' };
+        let petCarouselItems = dogObj.imgs.map(function (img) {
+            let obj = { src: img , altText: dogObj.name, caption: '' };
             return obj;
         })
 
-        let ownerCarouselItems = ownerObj.images.map(function (img) {
-            let obj = { src: 'url(' + img + ')', altText: ownerObj.name, caption: '' };
+        let ownerCarouselItems = ownerObj.imgs.map(function (img) {
+            let obj = { src: img, altText: ownerObj.name, caption: '' };
             return obj;
         })
 
@@ -144,13 +145,9 @@ export default class Card extends Component {
                                     </div>
                                     <div className="card-body">
                                         <h3 className="card-title name">{dogObj.name + ', ' + dogObj.age}</h3>
-<<<<<<< HEAD
                                         <p className="card-text breed">{dogObj.gender + ', ' + dogObj.breed}</p>
                                         <p className='card-text bio'>{this.props.user.bio}</p>
-=======
                                         <p className="card-text breed">{dogObj.sex + ', ' + dogObj.breed}</p>
-                                        <p className='card-text bio'>{dogObj.bio}</p>
->>>>>>> parent of 18b80da... card updates
                                         <div className='row'>
                                             <div className='col justify-content-center'>
                                                 <button className={css(styles.btnLike, styles.btnNope)} onClick={(event) => 
@@ -187,11 +184,8 @@ export default class Card extends Component {
                                     </div>
                                     <div className="card-body">
                                         <h3 className="card-title name">{ownerObj.name + ', ' + ownerObj.age}</h3>
-<<<<<<< HEAD
                                         <p className="card-text breed">{ownerObj.occupation}</p>
-=======
-                                        <p className="card-text breed">{ownerObj.occupation}</p>
->>>>>>> fixed broken
+
                                         <p className='card-text bio'>{this.props.user.bio}</p>
                                         <div className='row'>
                                             <div className='col justify-content-center'>
